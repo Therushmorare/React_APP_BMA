f360b81304a5e6f7f0b5aaa3fe76845ae418c4f7
therushmorare <tshego.tt.morare@gmail.com>
2025-10-23 03:52:14 +0200

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,19 @@
-import "@/app/styles/globals.css"
-import Layout from "@/app/components/Layout.jsx"
+import "@/app/styles/globals.css";
+import Layout from "@/app/components/Layout.jsx";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, params }) {
+  // Check the current path
+  const path = typeof window !== "undefined" ? window.location.pathname : "";
+
+  const noLayoutPages = ["/login", "/mfa"];
+
+  const useLayout = !noLayoutPages.includes(path);
+
   return (
     <html lang="en">
       <body>
-        <Layout>
-          {children}
-        </Layout>
+        {useLayout ? <Layout>{children}</Layout> : children}
       </body>
     </html>
   );
 }
-
